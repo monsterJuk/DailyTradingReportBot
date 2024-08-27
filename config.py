1eb950357aca22ee9ff5ea13a1037fabb31e611f
monster <musakovich@gmail.com>
2024-08-28 01:51:40 +0500

    add config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# @monster_info_bot token
+TOKEN = '7447900167:AAECFhchzfvDM8Z77jRzLwvez-jyw-D09CI'
+
+# MEXC API keys
+API_KEY = 'mx0vglioP2OL5Hv8pQ'
+SECRET_KEY = '4de0b4b1ed9c4e769a2703503eca5fca'
